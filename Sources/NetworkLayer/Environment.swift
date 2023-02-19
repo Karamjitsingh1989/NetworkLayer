54935cc5c4b1b772ec8789ad9002c445254cc981
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-02-19 22:17:33 +0530

    Added Environment

---
+++ Sources/NetworkLayer/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Karamjit Singh on 19/02/23.
+//
+
+import Foundation
+
+public enum Environment: String, CaseIterable {
+    case development
+    case staging
+    case production
+}
